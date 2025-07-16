165b7dfeee921be2fafe06289a55f719d3bc05b0
Sebastian <133876094+sebastianpod@users.noreply.github.com>
2025-07-16 11:06:12 +0200

    Update 002_Ch4113ng3.py

--- 7kyu/002_Ch4113ng3.py
+++ 7kyu/002_Ch4113ng3.py
@@ -1,14 +1,6 @@
 # Ch4113ng3
 
 # ---
-# ## Task Description
-# Write a function that replaces specific letters in a given string with numbers:
-# 'a' or 'A' should be replaced with '4'.
-# 'e' or 'E' should be replaced with '3'.
-# 'l' should be replaced with '1'.
-# Other characters should remain unchanged.
-
-# ---
 # ## CodeWars Description
 # Ch4113ng3
 # Make your strings more nerdy: Replace all 'a'/'A' with 4, 'e'/'E' with 3 and 'l' with 1 e.g. "Fundamentals" --> "Fund4m3nt41s"
