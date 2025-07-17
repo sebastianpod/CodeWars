dc919c11449647cb372df6a6cf2d3cf4ea95e62f
Sebastian <133876094+sebastianpod@users.noreply.github.com>
2025-07-17 22:37:46 +0200

    Create 016_reverse_string.py

---
+++ 8kyu/016_reverse_string.py
@@ -0,0 +1,13 @@
+# reverse string
+
+# ---
+# ## CodeWars Description
+# Complete the solution so that it reverses the string passed into it.
+# 'world'  =>  'dlrow'
+# 'word'   =>  'drow'
+
+# ---
+# ## Solution
+
+def solution(string):
+    return string[::-1]
