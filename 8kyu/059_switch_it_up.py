cf6477ac51c47a75047febebacc7412ac830efa1
Sebastian <133876094+sebastianpod@users.noreply.github.com>
2025-07-19 23:27:15 +0200

    Create 059_switch_it_up.py

---
+++ 8kyu/059_switch_it_up.py
@@ -0,0 +1,15 @@
+# Switch it Up!
+
+# ---
+# ## CodeWars Description
+# When provided with a number between 0-9, return it in words. Note that the input is guaranteed to be within the range of 0-9.
+# Input: 1
+# Output: "One".
+# If your language supports it, try using a switch statement.
+
+# ---
+# ## Solution
+
+def switch_it_up(number):
+    char = ['zero' , 'one' , 'two' , 'three' , 'four' , 'five' , 'six' , 'seven' , 'eight' , 'nine']
+    return char[number].capitalize()
